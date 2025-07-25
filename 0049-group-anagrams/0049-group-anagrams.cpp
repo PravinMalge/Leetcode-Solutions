4b6e7b9cb9d68ba36c5f99005b7d3cb7f8e254cb
Pravin Malge <pravinmalge123@gmail.com>
2025-07-25 18:54:33 +0530

    Time: 18 ms (53.14%), Space: 25 MB (65.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map<string, vector<string>> mp;
 
-        for(auto str: strs){
+        for (auto str : strs) {
             string s = str;
             sort(s.begin(), s.end());
             mp[s].push_back(str);
@@ -11,7 +11,7 @@ public:
 
         vector<vector<string>> ans;
 
-       for(auto i = mp.begin(); i != mp.end(); i++){
+        for (auto i = mp.begin(); i != mp.end(); i++) {
             ans.push_back(i->second);
         }
 
