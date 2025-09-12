5f5ff8a9fa91a9f29ac25f4897f63e636b227531
Pravin Malge <pravinmalge123@gmail.com>
2025-09-12 15:38:04 +0530

    Time: 0 ms (100%), Space: 12.7 MB (6.27%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-
-   void removeOcc(string &s, string &part){
+    void removeOcc(string& s, string& part) {
         int found = s.find(part);
-        if(found != string::npos){
-            string leftPart = s.substr(0,found);
+        if (found != string::npos) {
+            string leftPart = s.substr(0, found);
             string rightPart = s.substr(found + part.size(), s.size());
             s = leftPart + rightPart;
-            
+
             removeOcc(s, part);
-        }
-        else{
+        } else {
             return;
         }
     }
 
     string removeOccurrences(string s, string part) {
-        removeOcc(s,part);
+        removeOcc(s, part);
         return s;
     }
 };
