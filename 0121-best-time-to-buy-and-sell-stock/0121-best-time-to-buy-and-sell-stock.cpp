40dd150eb2aa40476fed07d4750c866482a0dd71
Pravin Malge <pravinmalge123@gmail.com>
2025-09-14 12:01:41 +0530

    Time: 0 ms (100%), Space: 97.5 MB (25.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
+    void maxProfitFinder(vector<int>& prices, int i, int&minPrice, int&maxProfit){
+        //Base Case
+        if(i == prices.size()) return;
+
+        //Solution
+        if(prices[i] < minPrice) minPrice = prices[i];
+        int todaysProfit = prices[i] - minPrice;
+        if(todaysProfit > maxProfit) maxProfit = todaysProfit;
+
+        //RE
+        maxProfitFinder(prices, i+1, minPrice, maxProfit);
+    }
+
     int maxProfit(vector<int>& prices) {
-      int maxProfit = 0;
-      int minPrice = prices[0];
+        int minPrice = INT_MAX;
+        int maxProfit = INT_MIN;
+        maxProfitFinder(prices, 0, minPrice, maxProfit);
 
-      for(int i = 1; i <  prices.size(); i++)
-      {
-            if(prices[i] > minPrice)
-            {
-                maxProfit = max(maxProfit, prices[i] - minPrice);
-            }
-            minPrice = min(minPrice, prices[i]);
-      }  
+        return maxProfit;
 
-         return maxProfit;
+
+    // Iterative solution
+    //   int maxProfit = 0;
+    //   int minPrice = prices[0];
+
+    //   for(int i = 1; i <  prices.size(); i++)
+    //   {
+    //         if(prices[i] > minPrice)
+    //         {
+    //             maxProfit = max(maxProfit, prices[i] - minPrice);
+    //         }
+    //         minPrice = min(minPrice, prices[i]);
+    //   }  
+
+    //      return maxProfit;
     }
 };
